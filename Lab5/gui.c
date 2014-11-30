f3d71d395537552f7537046a015bc641fcca06ff
David Yao <excsniper@gmail.com>
2014-11-30 17:04:41 -0500

    Refactor and comments of gui.c

--- Lab5/gui.c
+++ Lab5/gui.c
@@ -9,8 +9,6 @@
 #include "gui.h"
 
 static uint8_t vol = (STA_VOL_MAX-STA_VOL_MIN)/2;
-static uint8_t status = GUI_STATUS_STOPPED;
-static char *cur_song;
 
 void GUI_init()
 {
@@ -24,6 +22,7 @@ void GUI_init()
     GPIOB->IBE &= ~(1<<0 | 1<<1 | 1<<5);
     GPIOB->IEV &= ~(1<<0 | 1<<1 | 1<<5);
     GPIOB->DEN |= 1<<0 | 1<<1 | 1<<5;
+    GPIOB->ICR |= 1<<0 | 1<<1 | 1<<5;  // clear the interrupt since RIS will be used
     
     // SW5 & SW6
     SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R4;
@@ -35,6 +34,7 @@ void GUI_init()
     GPIOE->IBE &= ~(1<<4 | 1<<5);
     GPIOE->IEV &= ~(1<<4 | 1<<5);
     GPIOE->DEN |= 1<<4 | 1<<5;
+    GPIOB->ICR |= 1<<4 | 1<<5;
     
     // LCD_PWM
     SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R5;
@@ -54,6 +54,10 @@ typedef __packed struct File
 
 File_t* get_files(const TCHAR* path)
 {
+    /**
+        This function dynamically creates a doubly-linked list of only mp3 files
+        on the heap until its read all of them or we run out of heap space
+    */
     DIR dir;
     FILINFO fno;
     uint16_t len;
@@ -68,19 +72,18 @@ File_t* get_files(const TCHAR* path)
     if (f_opendir(&dir, path) != FR_OK)
         return NULL;
     
-    for(;;)
+    // while we can successfully read the dir and it's not the end
+    while ( (f_readdir(&dir, &fno) == FR_OK) && (fno.fname[0] != '\0') )
     {
-        if ( (f_readdir(&dir, &fno) != FR_OK) || (fno.fname[0] == '\0') )
-            break;
-        
+
 #if _USE_LFN
             fn = *fno.lfname ? fno.lfname : fno.fname;
 #else
             fn = fno.fname;
 #endif
-        
+
         len = strlen(fn);
-        
+
         // skip dir and non mp3 files
         if ( (fno.fattrib & AM_DIR) ||
              (len < 5)              ||
@@ -90,17 +93,21 @@ File_t* get_files(const TCHAR* path)
         // allocate space for size of File_t struct and file name
         if ((cur_fp = (File_t*) malloc(sizeof(File_t)+len)) == NULL)
             break;
-        
-        // copy the fname to the address right after Filt_t
+
+        // point fname to the address right after Filt_t
         cur_fp->fname = (char*)cur_fp+sizeof(File_t);
+        // copy the fname
+        strcpy(cur_fp->fname, fn);
+        // cur_fp is the end of the list
         cur_fp->next = NULL;
-        strcpy(cur_fp->fname, fn);        
-        
+
+        // if the list is empty
         if (first_fp == NULL)
         {
             first_fp = cur_fp;
             first_fp->prev = NULL;
         }
+        // if there's only one item in the list
         else if (prev_fp == NULL)
         {
             first_fp->next = prev_fp = cur_fp;
@@ -112,9 +119,11 @@ File_t* get_files(const TCHAR* path)
             cur_fp->prev = prev_fp;
             prev_fp = cur_fp;
         }
-    }
+    }  // while (...)
     
     f_closedir(&dir);
+    
+    // return the head of the linked list
     return first_fp;
 }
     
@@ -123,26 +132,44 @@ File_t* get_files(const TCHAR* path)
     
 void GUI_Thread (void const *argument)
 {
+    /**
+        This GUI makes the assumes that, during operation, the SD card will not be removed and
+        the contects of the SD card will not be changed. It also assumes that all of mp3 files
+        are on the top dir and there's subdirs are not supported.
+    */
+
+    static uint8_t status = GUI_STATUS_STOPPED;
+    static char *cur_song;
+
     File_t *cur_fp;
     osEvent sig;
-    
+
     // get the list of mp3s
     cur_fp = get_files("");
-    
-    // display the first song
+
     LCD_clear_1();
+    // see if there's at least one song
     if (cur_fp != NULL)
     {
+        // if so, display the song
         LCD_write_str(cur_fp->fname, LCD_DDRAM_LINE1_ADDR);
     }
     else
     {
+        // otherwise display an error msg and quit
         LCD_write_str("<No Songs Found>", LCD_DDRAM_LINE1_ADDR);
         return;
     }
-    
+
     while(1)
-    {        
+    {
+        /** 
+            below are the code to handle button presses. 
+            the buttons are polled because it greatly simplifies code dev
+            (interrupts and RTOSes can get a bit complicated)
+            and the real time requirements are very lenient.
+        */
+            
         if (GPIOB->RIS & (1<<0))
         {
             /* Vol+ (SW2) */
@@ -232,7 +259,7 @@ void GUI_Thread (void const *argument)
             // clear int
             GPIOE->ICR |= 1<<5;
         }
-        
+
         // check if the player is done playing
         sig = osSignalWait(GUI_SIG_NEXT, 0);
         if ( (sig.status == osEventSignal) && (sig.value.signals & GUI_SIG_NEXT) )
@@ -255,7 +282,7 @@ void GUI_Thread (void const *argument)
                 status = GUI_STATUS_STOPPED;
             }
         }
-        
+
         osDelay(GUI_DELAY);
     }
 }
